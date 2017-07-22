2a81ee585192a1a94477cde65811944ac7acaaf9
James Beasley <jbeasley@alumni.cmu.edu>
2017-07-22 15:57:08 -0500

    n/a

--- sdcp9/src/PID.cpp
+++ sdcp9/src/PID.cpp
@@ -2,20 +2,38 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
+//constructor
 PID::PID() {}
 
+//destructor
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+//set control gain for each term
+void PID::Init(double Kp, double Ki, double Kd)
+{
+    //set gains
+    this->Kp = Kp; //set proportional gain
+    this->Ki = Ki; //set integral gain
+    this->Kd = Kd; //set derivative gain
+
+    //zero errors
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+
+void PID::UpdateError(double cte)
+{
+    d_error = cte - p_error; //p_error contains our previous cte so by calculating derivative term first we don't need to track previous cte as a separate variable
+    p_error = cte;
+    i_error += cte;
 }
 
-double PID::TotalError() {
+
+double PID::TotalError()
+{
+    //(-tau_p * cte) - (tau_d * diff_cte) - (tau_i * total_cte)
+    return (-Kp * p_error) - (Kd * d_error) - (Ki * i_error);
 }
 
--- sdcp9/src/main.cpp
+++ sdcp9/src/main.cpp
@@ -32,8 +32,16 @@ int main()
 {
   uWS::Hub h;
 
+  //init the PID class
   PID pid;
-  // TODO: Initialize the pid variable.
+
+  //init the control gains
+  double Kp = 0.2;
+  double Ki = 0.004;
+  double Kd = 3.0;
+
+  //set the control gains
+  pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,6 +66,12 @@ int main()
           * another PID controller to control the speed!
           */
           
+          //update cross track error
+          pid.UpdateError(cte);
+
+          //get back steering value
+          steer_value = pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
